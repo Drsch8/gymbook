a1c264e75ff96e8c2a45f72cd0407c57b3983841
dasc <dasc@mail.de>
2026-04-11 14:45:32 +0200

    scroll3

--- src/components/SetRow.tsx
+++ src/components/SetRow.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import type { ExerciseSet, TrackingType, WeightUnit } from '../types'
 import { dragState } from '../utils/dragState'
 
--- src/utils/dragState.ts
+++ src/utils/dragState.ts
@@ -6,5 +6,5 @@ const _listeners = new Set<() => void>()
 export const dragState = {
   get(): DragInfo { return _state },
   set(s: DragInfo) { _state = s; _listeners.forEach(l => l()) },
-  subscribe(l: () => void) { _listeners.add(l); return () => _listeners.delete(l) },
+  subscribe(l: () => void) { _listeners.add(l); return () => { _listeners.delete(l) } },
 }
